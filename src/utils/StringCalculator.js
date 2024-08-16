54933d4eb74d7c959972a1e3a58e6a8a893d67c8
Devmuliya16 <muliyadev16@gmail.com>
2024-08-16 15:21:30 +0530

    updated string calculator and test cases

--- src/utils/StringCalculator.js
+++ src/utils/StringCalculator.js
@@ -1,12 +1,23 @@
 class StringCalculator {
-    delemeters = [',','\n'];
-    add(numbers){
-        if(!numbers)
+    delemeters = [',', '\n'];
+    add(numbers) {
+        if (!numbers)
             return 0;
-        const nums = numbers.split(new RegExp(`[${this.delemeters.join('')}]`)).map(Number);
-        return nums.reduce((sum,num)=>sum+num,0);
+        let numString = numbers;
+        if (numbers.startsWith('//')) {
+            const delemetersEndInd = numbers.indexOf('\n');
+            const customDelimiter = numbers.substring(2, delemetersEndInd);
+            this.delemeters.push(customDelimiter);
+            numString = numbers.substring(delemetersEndInd + 1);
+        }
+        const nums = numString.split(new RegExp(`[${this.delemeters.join('')}]`)).map(Number);
+        const negatives = nums.filter(num => num < 0);
+        if (negatives.length) {
+            throw new Error(`Negative numbers not allowed: ${negatives.join(', ')}`);
+        }
+        return nums.reduce((sum, num) => sum + num, 0);
     }
-    
+
 }
 
 export default StringCalculator
--- test/utils/StringCalculator.test.js
+++ test/utils/StringCalculator.test.js
@@ -4,39 +4,75 @@ import { describe, it ,beforeEach } from 'mocha';
 import StringCalculator from "../../src/utils/StringCalculator.js";
 
 
-describe("String Calculator - Simple Addition tests", ()=>{
+describe("String Calculator test suit", ()=>{
     let calculator;
     beforeEach(()=>{
         calculator = new StringCalculator();
     })
 
-    it("Should return 0 for an empty string", ()=>{
-        expect(calculator.add("")).to.equal(0)
+    describe('Simple addition tests ', () => { 
+        it("Should return 0 for an empty string", ()=>{
+            expect(calculator.add("")).to.equal(0)
+        });
+    
+        it('should return the number itself for a single number', () => {
+            expect(calculator.add('1')).to.equal(1);
+            expect(calculator.add('5')).to.equal(5);
+        });
+    
+        it('should return the sum of two numbers', () => {
+            expect(calculator.add('1,2')).to.equal(3);
+            expect(calculator.add('3,5')).to.equal(8);
+        });
+    
+        it('should return the sum of multiple numbers', () => {
+            expect(calculator.add('1,2,3')).to.equal(6);
+            expect(calculator.add('4,5,6,7')).to.equal(22);
+        });
+
+        it('should return the sum of multiple numbers with multiple consicutive delimiters', () => {
+            expect(calculator.add('1,2,,3,\n,')).to.equal(6);
+            expect(calculator.add('1,5,,,,3,,')).to.equal(9);
+            expect(calculator.add('4,\n5,\n6,7,')).to.equal(22);
+        });
     });
 
-    it('should return the number itself for a single number', () => {
-        expect(calculator.add('1')).to.equal(1);
-        expect(calculator.add('5')).to.equal(5);
-    });
+    describe('Custom delimitors test', () => { 
+        it('should handle new lines between numbers', () => {
+            expect(calculator.add('1\n2,3')).to.equal(6);
+            expect(calculator.add('4\n5\n6')).to.equal(15);
+        });
+    
+        it('should handle custom delimiters', () => {
+            expect(calculator.add('//;\n1;2')).to.equal(3);
+            expect(calculator.add('//|\n2|3|4')).to.equal(9);
+        });
 
-    it('should return the sum of two numbers', () => {
-        expect(calculator.add('1,2')).to.equal(3);
-        expect(calculator.add('3,5')).to.equal(8);
-    });
+        it('should handle same trailing of custom delimiters', () => {
+            expect(calculator.add('//;\n1;;;2')).to.equal(3);
+            expect(calculator.add('//|\n2|3|||4')).to.equal(9);
+        }); 
 
-    it('should return the sum of multiple numbers', () => {
-        expect(calculator.add('1,2,3')).to.equal(6);
-        expect(calculator.add('4,5,6,7')).to.equal(22);
-    });
+        it(`should work with custom delimitor and default delimitors ',' and '\\n'`, () => {
+            expect(calculator.add('//;\n1;;;2')).to.equal(3);
+            expect(calculator.add('//|\n2|3|,\n5,|4')).to.equal(14);
+        });          
+    })
 
-    it('should return the sum of multiple numbers with multiple consicutive delimiters', () => {
-        expect(calculator.add('1,2,,3,\n,')).to.equal(6);
-        expect(calculator.add('1,5,,,,3,,')).to.equal(9);
-        expect(calculator.add('4,\n5,\n6,7,')).to.equal(22);
-    });
+    describe('Negative numbers tests', () => { 
+        it('should throw an error for single negative number', ()=>{
+            expect(()=> calculator.add('-1').to.throw('Negative numbers not allowed: -1'));
+            expect(()=> calculator.add('-4').to.throw('Negative numbers not allowed: -4'));
+        })
 
-    it('should throw an error for negative numbers', () => {
-        expect(() => calculator.add('1,-2,3')).to.throw('Negative numbers not allowed: -2');
-        expect(() => calculator.add('//;\n1;-2;-3')).to.throw('Negative numbers not allowed: -2, -3');
+        it('should throw an error for negative numbers', () => {
+            expect(() => calculator.add('1,-2,3')).to.throw('Negative numbers not allowed: -2');
+            expect(() => calculator.add('1,-2,-3')).to.throw('Negative numbers not allowed: -2, -3');
+        });
+
+        it('should throw an error for negative numbers with custom delimitors', ()=>{
+            expect(() => calculator.add('//;\n1;-2;-3')).to.throw('Negative numbers not allowed: -2, -3');
+            expect(() => calculator.add('//$\n-1$-2$-3')).to.throw('Negative numbers not allowed: -1, -2, -3');
+        });
     });
-})
+});
